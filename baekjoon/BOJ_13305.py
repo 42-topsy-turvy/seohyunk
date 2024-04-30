aaac1bfc265548a5081bf0b6c0e2e58c435b99bf
se0hyun <kimlisa00@seoultech.ac.kr>
2024-04-30 23:23:02 +0900

    BOJ 13305 주유소

--- baekjoon/BOJ_13305.py
+++ baekjoon/BOJ_13305.py
@@ -5,9 +5,6 @@ N = int(sys.stdin.readline())
 length_list = list(map(int, sys.stdin.readline().split()))
 price_list = list(map(int, sys.stdin.readline().split()))
 
-# [2, 3, 1]
-# [5, 2, 4, 1]
-
 gas_price = price_list[0] * length_list[0]
 min_unit_price = price_list[0]
 
@@ -23,4 +20,4 @@ for i in range(1, len(price_list) - 1):
         gas_price += min_unit_price * length_list[i]
 #        print(i, "sum ", gas_price)
 
-print(i, gas_price)
+print(gas_price)
