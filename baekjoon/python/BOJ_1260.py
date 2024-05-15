4dd8ae878dd952c3b1384f0afbb8aa2ff7d96db1
se0hyun <kimlisa00@seoultech.ac.kr>
2024-05-15 14:52:21 +0900

    BOJ 1260 DFS와 BFS
    dictionary로 입력
    DFS: 재귀
    BFS: deque

--- baekjoon/python/BOJ_1260.py
+++ baekjoon/python/BOJ_1260.py
@@ -1,45 +1,57 @@
+# BOJ 1260 DFS와 BFS
 from collections import deque
+import sys
 
+def DFS(dictionary, start):
+    visited = []
+    def dfs_recursive(node):
+        if node not in visited:
+            visited.append(node)
+            for neighbor in sorted(dictionary.get(node, [])):
+                dfs_recursive(neighbor)
+    dfs_recursive(start)
+    return visited
 
-def dfs(start):
-    visited[start] = True
-    print(start, end=" ")
+def BFS(dictionary, start):
+    visited = []
+    queue = deque()
 
-    for i in graph[start]:
-        if not visited[i]:
-            dfs(i)
+    queue.append(start)
 
-
-def bfs(start):
-    queue = deque([start])
-    visited[start] = True
     while queue:
+        node = queue.popleft()
+        if node not in visited:
+            visited.append(node)
+            if node in dictionary:
+                queue.extend(sorted(dictionary[node]))  
+    return visited
 
-        v = queue.popleft()
-        print(v, end=" ")
-        for i in graph[v]:
-            if not visited[i]:
-                visited[i] = True
-                queue.append(i)
+def main():
+    N, M, V = map(int, sys.stdin.readline().split())
+    # dict 이용하기
+    node_dict = {}
 
+    # 리스트 이용하기
+    # node_list = [[]]
+    
+    for _ in range(M):
+        node1, node2 = map(int, sys.stdin.readline().split())
+        node_dict.setdefault(node1, []).append(node2)
+        node_dict.setdefault(node2, []).append(node1)
+        # setdefault(키 값, 값)
+        # 키 값이 있다면 키 값 반환, 없다면 두 번째 인자 반환
 
-N, M, V = map(int, input().split())
-graph = [[] for _ in range(N + 1)]
+    # while(M > 0):
+    #     node1, node2 = map(int, sys.stdin.readline().split())
+    #     if node1 not in node_dict:
+    #         node_dict[node1] = [node2]
+    #     else:
+    #         node_dict[node1].append(node2)
+    #     M -= 1
+    
 
-for _ in range(M):
-    a, b = map(int, input().split())
-    graph[a].append(b)
-    graph[b].append(a)
+    print(*DFS(node_dict, V))
+    print(*BFS(node_dict, V))
 
-# 정렬
-for i in graph:
-    i.sort()
-
-# dfs
-visited = [False] * (N + 1)
-dfs(V)
-print()
-
-# bfs
-visited = [False] * (N + 1)
-bfs(V)
+if __name__== "__main__":
+    main()
